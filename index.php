fee52182535907b3209c024c1afcaa1e50061463
tyler <tyler@TylersPC>
2024-03-25 17:18:05 -0400

    TEST

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>hi</h1>
+    </body>
+</html>
